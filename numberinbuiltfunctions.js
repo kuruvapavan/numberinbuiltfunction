9ea563012ad15764aad599776576559dec3a16da
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 18:24:35 +0530

    updated

---
+++ numberinbuiltfunctions.js
@@ -0,0 +1,19 @@
+var num=10;
+console.log(typeof(num))
+console.log(num.toString())
+console.log(typeof(num.toString()))
+
+var str='20';
+console.log(typeof(str))
+console.log(parseInt(str))
+console.log(typeof(parseInt(str)))
+
+var str=30.32;
+console.log(typeof(str))
+console.log(parseFloat(str))
+console.log(typeof(parseFloat(str)))
+
+var str=40;
+console.log(typeof(str))
+console.log(str.toFixed())
+console.log(typeof(str.toFixed()))
